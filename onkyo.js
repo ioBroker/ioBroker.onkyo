ff8d9377beb6cc29d30981729b0dd4fe67e5246b
GermanBluefox <dogafox@gmail.com>
2015-05-07 21:47:11 +0200

    (bluefox) fix errors

--- onkyo.js
+++ onkyo.js
@@ -16,7 +16,8 @@ var adapter = utils.adapter({    // name has to be set and has to be equal to ad
         adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
 
         if (!state.ack) {
-            var ids = id.split(".", 3)[2];
+            var ids = id.split(".");
+            ids = ids[ids.length - 1];
 
             if (ids == 'command') {
                 // Determine whether it's a raw or high-level command.
@@ -35,10 +36,17 @@ var adapter = utils.adapter({    // name has to be set and has to be equal to ad
                 } else if (newVal === false || newVal === 'false') {
                     newVal = "off";
                 }
-                eiscp.command(objects[id].native.command + "=" + newVal);
+                if (!objects[id]) {
+                    adapter.log.error('Unknown object: ' + id + ' or no connection');
+                } else {
+                    if (!objects[id].native || !!objects[id].native.command) {
+                        adapter.log.warn('non controllable state: ' + id);
+                    } else {
+                        eiscp.command(objects[id].native.command + "=" + newVal);
+                    }
+                }
             }
         }
-
     },
 
     // is called when adapter shuts down - callback has to be called under any circumstances!
@@ -68,12 +76,13 @@ function notifyCommand(cmdstring, value) {
         adapter.log.debug('Received: ' + cmdstring + '[' + value + ']');
     }
 
-    // Convert into boolean?
+    // Convert into boolean
     if (value == "on") {
         value = true;
     } else if (value == "off") {
         value = false;
     }
+
     var found = false;
     for (var id in objects) {
         if (objects[id].native.command == cmdstring) {
@@ -84,36 +93,35 @@ function notifyCommand(cmdstring, value) {
     }
 
     if (!found) {
-        adapter.getObject(cmdstring, function (err, obj) {
-            if (!obj) {
-                var type;
-                // detect automatically type of state
-                if (cmdstring.indexOf('volume') != -1) {
-                    type = 'media.volume';
-                } else if (cmdstring.indexOf('power') != -1) {
-                    type = 'button';
-                } else if (cmdstring.indexOf('source') != -1) {
-                    type = 'media.source';
-                } else {
-                    type = 'media';
-                }
+        var type;
+        // detect automatically type of state
+        if (cmdstring.indexOf('volume') != -1) {
+            type = 'media.volume';
+        } else if (cmdstring.indexOf('power') != -1) {
+            type = 'button';
+        } else if (cmdstring.indexOf('source') != -1) {
+            type = 'media.source';
+        } else {
+            type = 'media';
+        }
 
-                adapter.setObject(cmdstring, {
-                    common: {
-                        name: cmdstring,
-                        type: type
-                    },
-                    native: {
-                        command: cmdstring
-                    },
-                    type: 'number'
-                }, function (err, obj) {
+        adapter.log.info('Create new object: ' + adapter.namespace + '.' + cmdstring + ', type = ' + type);
 
-                });
-            }
+        objects[adapter.namespace + '.' + cmdstring] = {
+            _id: adapter.namespace + '.' + cmdstring,
+            common: {
+                name: cmdstring,
+                type: type
+            },
+            native: {
+                command: cmdstring
+            },
+            type: 'number'
+        };
+
+        adapter.setObject(cmdstring, objects[adapter.namespace + '.' + cmdstring], function (err, obj) {
+            adapter.setState(cmdstring, {val: value, ack: true});
         });
-
-        adapter.setState(cmdstring, {val: value, ack: true});
     }
 }
 
@@ -124,38 +132,46 @@ function main() {
         adapter.log.info("Error: " + e);
     });
 
+    // Try to read all states
+    adapter.getStatesOf(function (err, objs) {
+        if (objs) {
+            for (var i = 0; i < objs.length; i++) {
+                objects[objs[i]._id] = objs[i];
+            }
+        }
+
+        // Connect to receiver
+        eiscp.connect(options);
+    });
+
     eiscp.on('connect', function () {
         adapter.log.info('Successfully connected to AVR');
         adapter.setState('connected', {val: true, ack: true});
 
-        // Try to read initial values
-        adapter.getStatesOf('', '', function (err, objs) {
-            if (objs) {
-                for (var i = 0; i < objs.length; i++) {
-                    objects[objs[i]._id] = objs[i];
-                    if (objs[i].native && objs[i].native.values && objs[i].native.values.indexOf('query') != -1) {
-                        adapter.log.info('Initial query: ' + objs[i].native.command);
-                        eiscp.command(objs[i].native.command + "=query");
-                    }
-                }
-            }
-        });
-
         // Query some initial information
-        eiscp.raw('PWRQSTN'); // Returns Power State
-        eiscp.raw('MVLQSTN');
+        /*eiscp.raw('PWRQSTN'); // Returns Power State
+        eiscp.raw('MVLQSTN'); // Returns master volume
         eiscp.raw('SLIQSTN'); // Returns Current Input
         eiscp.raw('SLAQSTN'); // Returns Current Audio Selection
-        eiscp.raw('LMDQSTN'); // Returns Current Listening Mode
+        eiscp.raw('LMDQSTN'); // Returns Current Listening Mode*/
 
         eiscp.get_commands('main', function (err, cmds) {
-            adapter.log.info('Please send following info to developer: ' + JSON.stringify(cmds));
             cmds.forEach(function (cmd) {
                 eiscp.get_command(cmd, function (err, values) {
-                    adapter.log.info('Please send following info to developer: ' + cmd + ', ' + JSON.stringify(values));
+                    adapter.log.debug('Please send following info to developer: ' + cmd + ', ' + JSON.stringify(values));
                 });
             });
         });
+
+        setTimeout(function () {
+            // Try to read initial values
+            for (var id in objects) {
+                if (objects[id].native && objects[id].native.values && objects[id].native.values.indexOf('query') != -1) {
+                    adapter.log.info('Initial query: ' + objects[id].native.command);
+                    eiscp.command(objects[id].native.command + "=query");
+                }
+            }
+        }, 5000);
     });
 
     eiscp.on('close', function () {
@@ -184,6 +200,4 @@ function main() {
     } else {
         adapter.log.info('Starting AVR discovery');
     }
-
-    eiscp.connect(options);
 }
